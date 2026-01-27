47d0dae6cf09441b6f240a58f806ddc13e661223
gabochelapeuloche <gabochelapeuloche>
2026-01-27 22:51:42 +0100

    management of pre-existing homonyms vms

--- vm-setup.sh
+++ vm-setup.sh
@@ -71,6 +71,26 @@ if [[ "$VERBOSE" = true ]]; then
   printf '  - %s\n' "${VMS[@]}"
 fi
 
+# verification de la pre-existance de vm au meme noms
+for VM in "${VMS[@]}"; do
+  if multipass info "$VM" >/dev/null 2>&1; then
+    printf "\nLe nom $VM est déjà utilisé."
+    read -p "Souhaitez-vous la supprimer ? (Y/N): " confirm
+    if [[ "$confirm" =~ ^[Yy](es)?$ ]]; then
+      if ! multipass stop "$VM" >/dev/null 2>&1; then
+        echo "Erreur lors de l'arrêt de $VM (peut-être déjà arrêtée)."
+      fi
+      if ! multipass delete "$VM" >/dev/null 2>&1; then
+        echo "Erreur lors de la suppression de $VM."
+      fi
+      multipass purge >/dev/null 2>&1
+    else
+      echo "Annulation de la création des VMs."
+      exit 1
+    fi
+  fi
+done
+
 # Lancer les VMs
 for NODE in "${VMS[@]}"; do
   multipass launch "$OS_VERSION" \
@@ -82,37 +102,37 @@ done
 
 # Tester la communication entre VMs
 if [[ "$VERBOSE" = true ]]; then
-  echo "Test de la communication inter VMs :"
+  printf "\nTest de la communication inter VMs :"
 fi
 
 for NODE1 in "${VMS[@]}"; do
   for NODE2 in "${VMS[@]}"; do
     if [[ "$NODE1" != "$NODE2" ]]; then
-    
-      if [[ "$VERBOSE" = true ]]; then
-        echo ""
-      fi
       
       IP_NODE2=$(multipass exec "$NODE2" -- hostname -I | awk '{print $1}')
       
-      PING_RESULT=multipass exec "$NODE1" -- ping -c 3 "$IP_NODE2" 2>&1
+      PING_RESULT=$(multipass exec "$NODE1" -- ping -c 3 "$IP_NODE2" 2>&1)
       
-      if [[ echo "$PING_RESULT" | grep -q "0% packet loss" ]]; then
-        echo "$NODE1 --> $NODE2 : connexion reussie"
-      else
-        echo "$NODE1 --> $NODE2 : connexion echouee"
+      if [[ "$VERBOSE" = true ]]; then
+        if echo "$PING_RESULT" | grep -q "0% packet loss"; then
+          printf '  - %s\n' "$NODE1 --> $NODE2 : connexion reussie"
+        else
+          printf '  - %s\n' "$NODE1 --> $NODE2 : connexion echouee"
+        fi
       fi
     fi
   done
 done
 
 # Créer une snapshot des VMs
-if [[ "$VERBOSE" = true ]]; then
-  echo "Creation des snapshots :"
-fi
-
 if [[ "$SNAPSHOT" = true ]]; then
+  
+  if [[ "$VERBOSE" = true ]]; then
+    printf "\nCreation des snapshots :"
+  fi
+
   for NODE in "${VMS[@]}"; do
+    printf '  - %s\n' "$NODE"
     multipass stop "$NODE"
     multipass snapshot "$NODE" --name "${NODE}-bare" --comment "instance before kube installation"
     multipass start "$NODE"
