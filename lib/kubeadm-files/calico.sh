7e5963affc4afb3b4f83c064211526c5059bda5f
gabochelapeuloche <gabochelapeuloche>
2026-01-29 19:23:27 +0100

    delete of old files

--- lib/kubeadm-files/calico.sh
+++ lib/kubeadm-files/calico.sh
@@ -1,5 +1,3 @@
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
-
 kubectl apply -f custom-resources.yaml
--- lib/utils.sh
+++ lib/utils.sh
@@ -23,3 +23,67 @@ die() {
 require_cmd() {
   command -v "$1" &>/dev/null || die "$1 n'est pas installé"
 }
+
+# A function that gathers users parameters for customization
+user_inputs() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --help|-h)
+        usage
+        exit 0
+        ;;
+      --cp-number)
+        CP_NUMBER="$2"
+        if ! is_number "$CP_NUMBER"; then
+          echo "CP_NUMBER doit être un entier"
+          exit 1
+        fi
+        shift 2
+        ;;
+      --w-number)
+        W_NUMBER="$2"
+        if ! is_number "$W_NUMBER"; then
+          echo "W_NUMBER doit être un entier"
+          exit 1
+        fi
+        shift 2
+        ;;
+      --cpus)
+        CPUS="$2"
+        if ! is_number "$CPUS"; then
+          echo "CPUS doit être un entier"
+          exit 1
+        fi
+        shift 2
+        ;;
+      --memory)
+        MEMORY="$2"
+        if ! is_storage "$MEMORY"; then
+          echo "MEMORY doit être de la forme XG avec X un entier positif"
+          exit 1
+        fi
+        shift 2
+        ;;
+      --disk)
+        DISK="$2"
+        if ! is_number "$DISK"; then
+          echo "DISK doit être de la forme XG avec X un entier positif"
+          exit 1
+        fi
+        shift 2
+        ;;
+      --network)
+        NETWORK="$2"
+        shift 2
+        ;;
+      --verbose)
+        VERBOSE=true
+        shift
+        ;;
+      *)
+        echo "Option inconnue : $1"
+        exit 1
+      ;;
+    esac
+  done
+}
